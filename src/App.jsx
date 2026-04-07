56d05fe4cd8107893eba92b6644d5b083f3a6415
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-08 00:59:40 +0530

    Creating Trusted By component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -10,6 +11,7 @@ const App = () => {
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
+      <TrustedBy />
     </div>
   );
 };
