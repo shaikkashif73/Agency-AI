35fd823819eab2ffa1b65370b3fb17932ba78325
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-08 03:09:54 +0530

    Creating the Services Cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
+      <Services />
     </div>
   );
 };
