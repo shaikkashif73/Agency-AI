a2b8b1867574afc99338d1c5c582b0ab0fd6a37e
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-08 00:40:54 +0530

    Creating of Hero Component

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -4,7 +4,7 @@ import assets from "../assets/assets";
 const ThemeToggleBtn = ({ theme, setTheme }) => {
   useEffect(() => {
     const prefersDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)",
+      "(prefers-color-scheme: dark)",  
     ).matches;
     setTheme(theme || (prefersDarkMode ? "dark" : "light"));
   }, []);
