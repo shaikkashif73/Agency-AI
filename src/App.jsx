1f4f6280113abde47ee07e7892e9178ffe7807ce
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-08 18:40:26 +0530

    Creating Teams Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
+import Teams from "./components/Teams";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -16,6 +17,7 @@ const App = () => {
       <TrustedBy />
       <Services />
       <OurWork />
+      <Teams />
     </div>
   );
 };
