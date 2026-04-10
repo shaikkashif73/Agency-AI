61ae5403aa755baa37b454f595fe4dbbbdbfc24c
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-10 17:39:50 +0530

    Adding Animation to web page using "Framer Motion"

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Title from "./Title";
 import assets from "../assets/assets";
 import toast from "react-hot-toast";
+import { motion } from "motion/react";
 
 const ContactUs = () => {
   const onSubmit = async (event) => {
@@ -30,7 +31,11 @@ const ContactUs = () => {
     }
   };
   return (
-    <div
+    <motion.div
+      initial={"hidden"}
+      whileInView={"visible"}
+      viewport={{ once: true }}
+      transition={{ staggerChildren: 0.2 }}
       id="contact-us"
       className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700
        dark:text-white"
@@ -40,7 +45,11 @@ const ContactUs = () => {
         desc="From strategy to execution, we craft digital solutions that move your business forward."
       />
 
-      <form
+      <motion.form
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        viewport={{ once: true }}
         onSubmit={onSubmit}
         className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full
       "
@@ -89,8 +98,8 @@ const ContactUs = () => {
         >
           Submit <img src={assets.arrow_icon} className="w-4" alt="" />
         </button>
-      </form>
-    </div>
+      </motion.form>
+    </motion.div>
   );
 };
 
