99289027d90526d4b043ae6c58185a3851381643
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-10 10:15:30 +0530

    Creating Footer component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import assets from "../assets/assets";
 const Hero = () => {
   return (
     <div
-      id="hero "
+      id="hero"
       className="flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white"
     >
       <div className="inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full">
